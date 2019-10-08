3df28bc9ec6d98164a8fcdbc29a802dde2c7bc43
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2019-10-08 14:37:41 -0400

    initial

---
+++ linked_lists_3.c
@@ -0,0 +1,4 @@
+/*Modify the example above to add new nodes to the linked list such that
+the linked list is always kept in ascending order. */
+
+// Copy your code from link_lists_1 or link_lists_2 to get started!
